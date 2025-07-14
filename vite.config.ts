45acab715e94d0ad50951f42f3fce5c6b9bccbd2
asperusmaia <kistenmackermaia@gmail.com>
2025-07-13 22:43:50 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  // ===== A LINHA QUE VOCÊ PRECISA ADICIONAR É ESTA: =====
+  base: '/painel-cliente-final/', // Use o nome exato do seu novo repositório aqui
+
   server: {
     host: "::",
     port: 8080,
